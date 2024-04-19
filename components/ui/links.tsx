65d83d3d368fed6941d10cb447020e14fd07d0d8
NormanD <129342654+IKLEOSDG@users.noreply.github.com>
2024-04-19 14:33:00 +1000

    add link

--- components/ui/links.tsx
+++ components/ui/links.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Theme from "../layout/componentTheme";
 
 export function Links() {
-  return <Theme componentName="links" componentStyle="normal" />;
+  return <Theme componentName="links" componentStyle="normal"  componentReactView="NormalLink"/>;
 }
 export function EnhancedLinks() {
-  return <Theme componentName="links" componentStyle="enhanced" />;
+  return <Theme componentName="links" componentStyle="enhanced" componentReactView="EnhancedLink"/>;
 }
 export function ExpandLinks() {
-  return <Theme componentName="links" componentStyle="expand" />;
+  return <Theme componentName="links" componentStyle="expand" componentReactView="ExpandLink"/>;
 }
---
+++ public/components/links/normal/react/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export function NormalLink() {
+  return <Button variant="link">Read More</Button>
+}
+
+
+export default NormalLink;
+
