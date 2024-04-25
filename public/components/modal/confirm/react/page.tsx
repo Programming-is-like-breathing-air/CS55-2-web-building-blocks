233a99cf9102b1911159347f925c6d351f15de92
Yuqing200718 <yuqingwu200718@gmail.com>
2024-04-25 15:17:38 +1000

    Component Update

--- public/components/modal/confirm/react/page.tsx
+++ public/components/modal/confirm/react/page.tsx
@@ -31,7 +31,7 @@ export function ConfirmModal() {
                         Are you sure you want to submit your application? You will not be able to make changes after that.
                     </DialogDescription>
                     <DialogFooter className="flex justify-center space-x-4 mt-4">
-                        <Button variant="destructive" onClick={() => { toast({ description: "Message has been sent.", }); }}>Confirm</Button>
+                        <Button variant="default" onClick={() => { toast({ description: "Message has been sent.", }); }}>Confirm</Button>
                         <DialogClose asChild>
                             <Button variant="secondary">Cancel</Button>
                         </DialogClose>
