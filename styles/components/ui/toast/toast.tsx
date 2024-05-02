90cb0d4e5261e50db314a753d1690ca779227234
Yuqing200718 <yuqingwu200718@gmail.com>
2024-05-02 14:53:58 +1000

    Set default style of toast to show on the top right

--- styles/components/ui/toast/toast.tsx
+++ styles/components/ui/toast/toast.tsx
@@ -70,11 +70,10 @@ const Toast = React.forwardRef<
 >(({ className, variant, position = 'top-right', ...props }, ref) => {
   // 根据位置添加适当的CSS类
   const positionClasses = position === 'top-right' ? 
-  "fixed top-0 z-[100]" : " bottom-0 right-0 z-[100]";
+  "fixed top-0 left-50 z-[100]" : " bottom-0 right-0 z-[100]";
 
   // const baseClasses = "flex max-h-screen w-full flex-col-reverse p-4 sm:flex-col md:max-w-[420px]";
-
-
+  
   return (
     <ToastPrimitives.Root
       ref={ref}
@@ -93,7 +92,7 @@ const ToastAction = React.forwardRef<
   <ToastPrimitives.Action
     ref={ref}
     className={cn(
-      "inline-flex h-8 shrink-0 items-center justify-center rounded-md border bg-transparent px-3 text-sm font-medium transition-colors hover:bg-secondary focus:outline-none focus:ring-1 focus:ring-ring disabled:pointer-events-none disabled:opacity-50 group-[.destructive]:border-muted/40 group-[.destructive]:hover:border-destructive/30 group-[.destructive]:hover:bg-destructive group-[.destructive]:hover:text-destructive-foreground group-[.destructive]:focus:ring-destructive",
+       "inline-flex items-center justify-center rounded-md border bg-transparent px-3 text-sm font-medium transition-colors hover:bg-secondary focus:outline-none focus:ring-1 disabled:pointer-events-none disabled:opacity-50",
       className
     )}
     onClick={(e) => {
